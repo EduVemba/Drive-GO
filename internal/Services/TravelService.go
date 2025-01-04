a8377eccb469bb1e77e67ec329179269cbc6bb8c
EduVemba <eduvemba18@gmail.com>
2025-01-04 18:22:39 +0000

    fix: HandlerCreateTravelIntent now only works with auth

--- internal/Services/TravelService.go
+++ internal/Services/TravelService.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"d_uber_golang/internal/Database/MongoDB"
 	"d_uber_golang/internal/Database/PostgreSQL"
+	"database/sql"
 	"encoding/json"
 	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -32,6 +34,8 @@ func HandlerCreateTravelIntent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var user UserRequirements
+
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&user); err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
@@ -40,19 +44,32 @@ func HandlerCreateTravelIntent(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
-	user.TimeStamp = time.Now()
+	/*
+		if user.From == "" || user.To == "" || user.Email == "" {
+			http.Error(w, "Missing required fields", http.StatusBadRequest)
+			return
+		}
+	*/
 
-	w.Header().Set("Content-Type", "application/json")
+	user.SessionToken = r.Header.Get("Authorization")
+	if user.SessionToken == "" {
+		http.Error(w, "Unauthorized - Missing session token", http.StatusUnauthorized)
+		return
+	}
 
-	var firstName, lastName string
-	err := PostgreSQL.Db.QueryRow(`SELECT first_name, last_name WHERE sessiontoken = $1`, user.SessionToken).
-		Scan(&firstName, &lastName)
+	//	user.SessionToken = user.SessionToken[len("Bearer "):]
+
+	tokenString := strings.TrimPrefix(user.SessionToken, "Bearer ")
+
+	var firstName, lastName, email string
+	err := PostgreSQL.Db.QueryRow(`SELECT first_name, last_name, email FROM requester WHERE sessiontoken = $1`, tokenString).
+		Scan(&firstName, &lastName, &email)
 
 	if err != nil {
-		if err.Error() == "no rows in result set" {
+		if err == sql.ErrNoRows {
 			http.Error(w, "Unauthorized - Invalid session token", http.StatusUnauthorized)
 		} else {
-			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			http.Error(w, "Database Error", http.StatusBadRequest)
 		}
 		return
 	}
@@ -60,6 +77,7 @@ func HandlerCreateTravelIntent(w http.ResponseWriter, r *http.Request) {
 	response := map[string]interface{}{
 		"first_name": firstName,
 		"last_name":  lastName,
+		"email":      email,
 		"from":       user.From,
 		"to":         user.To,
 		"time_stamp": user.TimeStamp.Format(time.RFC3339), // Formating to JSON
@@ -70,6 +88,7 @@ func HandlerCreateTravelIntent(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
